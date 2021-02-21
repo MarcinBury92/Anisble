0845e9c087ae3fd77d4bf8a9b470ab5f5722a5df
MarcinBury92 <49837506+MarcinBury92@users.noreply.github.com>
2021-02-21 08:20:49 +0100

    Delete SymbolickLink.sh

--- SymbolickLink.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-#This script uses the AD-HOC command to create symbolic links on managed hosts.
-
-ansible -m file -a "path=/root/vehicle/truck state=directory" Poland --become
-ansible -m file -a "path=/root/vehicle/truck/BrakesTest mode=g+rwx,o+rx owner=ansibleuser group=ansibleuser state=touch" Poland --become
-ansible -m file -a "src=/root/vehicle/truck/BrakesTest dest=/root/BrakesTest state=link" Poland --become
-
