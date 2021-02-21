2e255b4c8599e784302fe4ffaf0c83370927881f
Marcin <Marcingeo92@gmail.com>
2021-02-21 09:22:17 +0100

    Creating Symbolic Link using bash script and ansible ad-hoc command

---
+++ Symbolic_Link.sh
@@ -0,0 +1,8 @@
+
+
+#! /bin/bash
+#This script uses the AD-HOC command to create symbolic links on managed hosts.
+ansible -m file -a "path=/root/vehicle/truck state=directory" Poland --become
+ansible -m file -a "path=/root/vehicle/truck/BrakesTest mode=g+rwx,o+rx owner=ansibleuser group=ansibleuser state=touch" Poland --become
+ansible -m file -a "src=/root/vehicle/truck/BrakesTest dest=/root/BrakesTest state=link" Poland --become
+ansible -a "ls -l /root" Poland --become
